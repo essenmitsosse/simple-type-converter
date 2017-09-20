4812c4ed88b5eb90433535bd93b7d176cbc52ba9
marcus <marcus@essenmitsosse.de>
2017-09-20 12:40:06 +0200

    Refactor

--- simple-type-converter.js
+++ simple-type-converter.js
@@ -1,23 +1,10 @@
+"use strict";
+
 function toBoolean( value ) {
-	switch ( value ) {
-	case "0":
-		return false;
-	case "1":
-		return true;
-	case "false":
-		return false;
-	case "true":
-		return true;
-	case 0:
-		return false;
-	case 1:
-		return true;
-	case false:
-		return false;
-	case true:
-		return true;
-	}
-	return null;
+	value = String( value )
+		.toLowerCase();
+
+	return value === "undefined" ? false : Boolean( JSON.parse( value ) );
 }
 
 function toNumber( value ) {
@@ -28,27 +15,13 @@ function toString( value ) {
 	return String( value );
 }
 
-function convert( value, targetType ) {
-	var convertFunction;
-
-	switch ( targetType ) {
-	case "boolean":
-		convertFunction = toBoolean;
-		break;
-	case "number":
-		convertFunction = toNumber;
-		break;
-	case "string":
-		convertFunction = toString;
-		break;
-	}
-
-	return convertFunction( value );
-}
-
 module.exports = {
 	toBoolean: toBoolean,
 	toNumber: toNumber,
 	toString: toString,
-	convert: convert
+	typeNames: {
+		string: toString,
+		number: toNumber,
+		boolean: toBoolean
+	}
 };
